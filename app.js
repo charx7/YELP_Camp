e16f5049cb61a64005a62a6d18095926ddab9535
Carlos Huerta <carlos.huertaso@udlap.mx>
2017-12-03 18:04:54 -0600

    Ruteo basico aniadido y desplegado de render con un arreglo (falta implementar la BDD)

--- app.js
+++ app.js
@@ -5,9 +5,30 @@ var app = express();
 var bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
 
+// Definimos temporalmente uno arreglo con sitios para acampar en formato objeto de javacript
+var campamentos = [
+    {
+        nombreCampamento: "Guslanding",
+        imagenCampamento: "http://cdn.traveler.es/uploads/images/thumbs/es/trav/2/s/2016/02/deja_que_te_invada_el_espiritu_de_los_60_12_ideas_para_unas_vacaciones_hippies_183389546_1200x800.jpg"
+    },
+    {
+        nombreCampamento: "LolXDMafiaCampamento",
+        imagenCampamento: "https://assets.vice.com/content-images/contentimage/no-slug/d8582d38918fa398a67e269684fefb67.jpg"
+    }
+]
+
 // para definir el timpo de template a usar
 app.set("view engine", "ejs");
 
+// Definimos el routing para la HOME page
+app.get("/", function(request, response){
+    response.render("landing");
+});
+
+app.get("/sitiosparaacampar", function(request, response){
+    // Pasa el objeto de arreglo de campamentos
+    response.render("sitiosparaacampar", {campamentos: campamentos});
+});
 
 
 // Listener para establecer puerto
